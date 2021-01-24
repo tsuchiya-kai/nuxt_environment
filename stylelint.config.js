4bf19f4a65cc900ced5d4a86256c7f41562dd86e
tsuchiya kaita <tsuchiyakaita@arsaga.jp>
2021-01-24 19:09:44 +0900

    feat(global): stylelintの設定

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,8 +1,22 @@
 module.exports = {
-  extends: ['stylelint-config-standard', 'stylelint-config-prettier'],
+  extends: [
+    'stylelint-config-standard', // 最初からあった
+    'stylelint-config-prettier', // 最初からあった
+    'stylelint-config-recess-order', // プロパティの順番
+    'stylelint-rscss/config', // RSCSS用
+  ],
   // add your custom config here
   // https://stylelint.io/user-guide/configuration
   rules: {
+    // 'unicode-bom': 'never',
+    'max-nesting-depth': [
+      1,
+      {
+        ignore: ['pseudo-classes'],
+        ignoreAtRules: ['include', 'media'],
+      },
+    ],
     'at-rule-no-unknown': [true, { ignoreAtRules: ['mixin', 'if', 'for'] }],
   },
+  ignoreFiles: ['**/*.vue'], // NOTE: vueのスタイルは一旦無視で
 };
