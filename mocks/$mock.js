8829a1a19251fcdb7c0e912c562408e501f3fc20
tsuchiya-kai <yukaigangan@gmail.com>
2021-06-17 02:18:23 +0900

    feat(mock): mockの移植忘れを移植

--- mocks/$mock.js
+++ mocks/$mock.js
@@ -1,5 +1,15 @@
 /* eslint-disable */
 import mockServer from 'axios-mock-server'
+import mock0 from './todos/delete/_todo_id'
+import mock1 from './todos/create'
 
 export default (client) => mockServer([
+  {
+    path: '/todos/delete/_todo_id',
+    methods: mock0
+  },
+  {
+    path: '/todos/create',
+    methods: mock1
+  }
 ], client, '')
